73db32813af9848d77377aba1b2587c2569e645b
Taylor Webb <tbwebb22@gmail.com>
2024-09-14 16:02:02 -0400

    DRY it up

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, MutableRefObject } from "react";
 import { supabase } from "../db/supabaseClient";
 import { Tables } from "../db/database.types";
 import { SVG, Container } from "@svgdotjs/svg.js"; // Import Container for typing
@@ -10,26 +10,62 @@ export default function Home() {
   const [error, setError] = useState<string | null>(null);
   const svgRef = useRef<HTMLDivElement | null>(null); // Reference for the SVG container
   const drawRef = useRef<Container | null>(null); // Use Container type from svg.js
-  const svgWidth = 1200;
-  const svgHeight = 600;
-
+  const svgWidth = 1800;
+  const svgHeight = 900;
 
   function generateRandomString(n: number): string {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
     const charactersLength = characters.length;
-  
+
     for (let i = 0; i < n; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-  
+
     return result;
   }
 
+  function drawPlayerLine(
+    draw: MutableRefObject<Container | null>,
+    xStart: number,
+    yStart: number,
+    horizontalLength: number,
+    text: string
+  ) {
+    if (!draw.current) return;
+
+    draw.current
+      .line(xStart, yStart, xStart + horizontalLength, yStart)
+      .stroke({ width: 2, color: "#000000" });
+
+    // Calculate the center of the line for text placement
+    const textSize = horizontalLength / 10;
+    const textX = xStart + horizontalLength / 2;
+    const textY = yStart - 0.7 * textSize; // Slightly above the line, adjust as necessary
+
+    // Add the text and center it
+    draw.current.text(text)
+    .move(textX, textY) // Position the text
+    .font({ size: textSize, anchor: 'middle' }) // Set font size and center it horizontally
+    .center(textX, textY); // Center the text on the horizontal line
+  }
+
+  function drawVerticalLine(
+    draw: MutableRefObject<Container | null>,
+    xStart: number,
+    yStart: number,
+    verticalLength: number
+  ) {
+    if (!draw.current) return;
+
+    draw.current
+      .line(xStart, yStart, xStart, yStart + verticalLength)
+      .stroke({ width: 2, color: "#000000" });
+  }
+
+
   useEffect(() => {
-
-
-
     const fetchRounds = async () => {
       try {
         const gameId = 7; // Example game ID
@@ -91,106 +127,65 @@ export default function Home() {
 
         const numberOfLines = rounds * 2;
         const matchWidth = svgWidth / (numberOfLines + 2);
-        const fontSize = svgWidth / 100;
         let yStart = 50; // Use this to set y margin
 
         for (let round = 1; round < rounds; round++) {
           console.log("--------");
-          const matchesEachSide = 2**(rounds - round) / 2;
-          const matchHeight = (svgHeight - yStart*2) / (matchesEachSide*2 - 1);
+          const matchesEachSide = 2 ** (rounds - round) / 2;
+          const matchHeight =
+            (svgHeight - yStart * 2) / (matchesEachSide * 2 - 1);
           const xLeftLeft = (round - 1) * matchWidth;
           const xLeftRight = xLeftLeft + matchWidth;
 
           const xRightRight = svgWidth - (round - 1) * matchWidth;
           const xRightLeft = xRightRight - matchWidth;
 
-          console.log("round: ", round);
-          console.log("matchHeight: ", matchHeight);
-
           // Draw matches on left side of bracket
           for (let match = 1; match <= matchesEachSide; match++) {
-            // console.log("player: ", player);
-            const y = 2 * matchHeight * (match-1) + yStart;
-            // console.log("y: ", y);
+            const y = 2 * matchHeight * (match - 1) + yStart;
 
             // draw top line of match
-            drawRef.current
-              .line(xLeftLeft, y, xLeftRight, y)
-              .stroke({ width: 2, color: '#000000' });
-
-            // Add username for top match player
-            drawRef.current.text(generateRandomString(10))
-              .move(xLeftLeft + 10, y - (1.5 * fontSize)) // Position the text in the center of the SVG
-              .font({ size: fontSize, fill: '#000000', family: 'Arial' }); // Customize font size, color, and family
+            drawPlayerLine(drawRef, xLeftLeft, y, matchWidth, generateRandomString(10));
 
             // draw bottom line of match
-            drawRef.current
-            .line(xLeftLeft, y + matchHeight, xLeftRight, y + matchHeight)
-            .stroke({ width: 2, color: '#000000' });
-
-            // Add username for bottom match player
-            drawRef.current.text(generateRandomString(10))
-            .move(xLeftLeft + 10, y + matchHeight - (1.5 * fontSize)) // Position the text in the center of the SVG
-            .font({ size: fontSize, fill: '#000000', family: 'Arial' }); // Customize font size, color, and family
+            drawPlayerLine(drawRef, xLeftLeft, y + matchHeight, matchWidth, generateRandomString(10));
 
             // draw connecting line of match
-            drawRef.current
-            .line(xLeftRight, y, xLeftRight, y + matchHeight)
-            .stroke({ width: 2, color: '#000000' });
+            drawVerticalLine(drawRef, xLeftRight, y, matchHeight);
           }
 
           // Draw matches on right side of bracket
           for (let match = 1; match <= matchesEachSide; match++) {
             // console.log("player: ", player);
-            const y = 2 * matchHeight * (match-1) + yStart;
+            const y = 2 * matchHeight * (match - 1) + yStart;
             // console.log("y: ", y);
 
             // draw top line of match
-            drawRef.current
-              .line(xRightLeft, y, xRightRight, y)
-              .stroke({ width: 2, color: '#000000' });
-
-            // Add username for top match player
-            drawRef.current.text(generateRandomString(10))
-            .move(xRightLeft + 10, y - (1.5 * fontSize)) // Position the text in the center of the SVG
-            .font({ size: fontSize, fill: '#000000', family: 'Arial' }); // Customize font size, color, and family
+            drawPlayerLine(drawRef, xRightLeft, y, matchWidth, generateRandomString(10));
 
             // draw bottom line of match
-            drawRef.current
-            .line(xRightLeft, y + matchHeight, xRightRight, y + matchHeight)
-            .stroke({ width: 2, color: '#000000' });
-
-            // Add username for bottom match player
-            drawRef.current.text(generateRandomString(10))
-            .move(xRightLeft + 10, y + matchHeight - (1.5 * fontSize)) // Position the text in the center of the SVG
-            .font({ size: fontSize, fill: '#000000', family: 'Arial' }); // Customize font size, color, and family
+            drawPlayerLine(drawRef, xRightLeft, y + matchHeight, matchWidth, generateRandomString(10));
 
             // draw connecting line of match
-            drawRef.current
-            .line(xRightLeft, y, xRightLeft, y + matchHeight)
-            .stroke({ width: 2, color: '#000000' });
+            drawVerticalLine(drawRef, xRightLeft, y, matchHeight);
           }
 
-          yStart += matchHeight/2;
+          yStart += matchHeight / 2;
         }
 
         // Draw left side of final match
-        drawRef.current
-          .line((rounds-1) * matchWidth, svgHeight / 2, rounds * matchWidth, svgHeight / 2)
-          .stroke({ width: 2, color: '#000000' });
+        drawPlayerLine(drawRef, (rounds - 1) * matchWidth, svgHeight/2, matchWidth, generateRandomString(10));
 
         // Draw right side of final match
-        drawRef.current
-        .line((rounds+2) * matchWidth, svgHeight / 2, (rounds + 3) * matchWidth, svgHeight / 2)
-        .stroke({ width: 2, color: '#000000' });
+        drawPlayerLine(drawRef, (rounds + 2) * matchWidth, svgHeight/2, matchWidth, generateRandomString(10));
 
         // Draw winner box
         const winnerBoxHeight = svgHeight / 6;
         drawRef.current
           .rect(matchWidth * 2, winnerBoxHeight)
-          .move(rounds * matchWidth, (svgHeight / 2) - (winnerBoxHeight / 2)) // Position the box at xLeft and y
-          .fill('none')
-          .stroke({ width: 2, color: '#000000' }); // Black stroke with width 2
+          .move(rounds * matchWidth, svgHeight / 2 - winnerBoxHeight / 2) // Position the box at xLeft and y
+          .fill("none")
+          .stroke({ width: 2, color: "#000000" }); // Black stroke with width 2
       }
     }
   }, [matches, rounds]);
