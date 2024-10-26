acf68748765806acfda0fdc29a3dc3b64f577b87
Taylor Webb <tbwebb22@gmail.com>
2024-10-26 15:27:10 -0400

    use real FC users

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,67 @@
 "use client";
-import { useEffect, useState, useRef, MutableRefObject } from "react";
-import { supabase } from "../db/supabaseClient";
-import { Tables } from "../db/database.types";
+import { useEffect, useRef, MutableRefObject, useState } from "react";
+// import { supabase } from "../db/supabaseClient";
+// import { Tables } from "../db/database.types";
 import { SVG, Container } from "@svgdotjs/svg.js"; // Import Container for typing
+import { mockPlayers } from "./mockPlayers";
 
 export default function Home() {
   const [rounds, setRounds] = useState<number>();
-  const [matches, setMatches] = useState<Tables<"matches">[]>();
-  const [error, setError] = useState<string | null>(null);
+  // const [matches, setMatches] = useState<Tables<"matches">[]>();
+  // const [error, setError] = useState<string | null>(null);
   const svgRef = useRef<HTMLDivElement | null>(null); // Reference for the SVG container
   const drawRef = useRef<Container | null>(null); // Use Container type from svg.js
   const svgWidth = 1800;
   const svgHeight = 900;
 
-  function generateRandomString(n: number): string {
-    const characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let result = "";
-    const charactersLength = characters.length;
+  useEffect(() => {
+    setRounds(3);
+  }, []);
 
-    for (let i = 0; i < n; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
+  // function generateRandomString(n: number): string {
+  //   const characters =
+  //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  //   let result = "";
+  //   const charactersLength = characters.length;
 
-    return result;
+  //   for (let i = 0; i < n; i++) {
+  //     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  //   }
+
+  //   return result;
+  // }
+
+  function getRandomPlayer() {
+    return mockPlayers[Math.floor(Math.random() * mockPlayers.length)];
   }
+
 
   function drawPlayerLine(
     draw: MutableRefObject<Container | null>,
     xStart: number,
     yStart: number,
     horizontalLength: number,
-    text: string
+    username: string,
+    profileImage: string,
+    move: number
   ) {
     if (!draw.current) return;
 
+    console.log(profileImage, move);
+
     draw.current
       .line(xStart, yStart, xStart + horizontalLength, yStart)
-      .stroke({ width: 2, color: "#000000" });
+      .stroke({ width: 2, color: "#e59eff" });
 
     // Calculate the center of the line for text placement
-    const textSize = horizontalLength / 10;
+    const textSize = horizontalLength / 12;
     const textX = xStart + horizontalLength / 2;
     const textY = yStart - 0.7 * textSize; // Slightly above the line, adjust as necessary
 
     // Add the text and center it
-    draw.current.text(text)
-    .move(textX, textY) // Position the text
-    .font({ size: textSize, anchor: 'middle' }) // Set font size and center it horizontally
+    draw.current.text(username)
+    .move(textX, textY)
+    .font({ size: textSize, anchor: 'middle', fill: "#e59eff" })
     .center(textX, textY); // Center the text on the horizontal line
   }
 
@@ -61,69 +75,68 @@ export default function Home() {
 
     draw.current
       .line(xStart, yStart, xStart, yStart + verticalLength)
-      .stroke({ width: 2, color: "#000000" });
+      .stroke({ width: 2, color: "#e59eff" });
   }
 
 
+  // useEffect(() => {
+  //   const fetchRounds = async () => {
+  //     try {
+  //       const gameId = 6; // Example game ID
+
+  //       // Query the database using Supabase client-side
+  //       const { data, error } = await supabase
+  //         .from("rounds")
+  //         .select("*")
+  //         .eq("game_id", gameId);
+
+  //       if (error) {
+  //         throw new Error(error.message);
+  //       }
+  //       console.log("round count: ", data.length);
+  //       setRounds(4);
+  //     } catch (err) {
+  //       setError((err as Error).message);
+  //     }
+  //   };
+
+  //   const fetchMatches = async () => {
+  //     try {
+  //       const gameId = 7; // Example game ID
+
+  //       // Query the database using Supabase client-side
+  //       const { data, error } = await supabase
+  //         .from("matches")
+  //         .select("*")
+  //         .eq("game_id", gameId)
+  //         .order("round_id", { ascending: true });
+
+  //       if (error) {
+  //         throw new Error(error.message);
+  //       }
+
+  //       setMatches(data || []);
+  //     } catch (err) {
+  //       setError((err as Error).message);
+  //     }
+  //   };
+
+  //   fetchRounds();
+  //   fetchMatches();
+  // }, []);
+
   useEffect(() => {
-    const fetchRounds = async () => {
-      try {
-        const gameId = 7; // Example game ID
-
-        // Query the database using Supabase client-side
-        const { data, error } = await supabase
-          .from("rounds")
-          .select("*")
-          .eq("game_id", gameId);
-
-        if (error) {
-          throw new Error(error.message);
-        }
-        console.log("round count: ", data.length);
-        setRounds(4);
-      } catch (err) {
-        setError((err as Error).message);
-      }
-    };
-
-    const fetchMatches = async () => {
-      try {
-        const gameId = 7; // Example game ID
-
-        // Query the database using Supabase client-side
-        const { data, error } = await supabase
-          .from("matches")
-          .select("*")
-          .eq("game_id", gameId)
-          .order("round_id", { ascending: true });
-
-        if (error) {
-          throw new Error(error.message);
-        }
-
-        setMatches(data || []);
-      } catch (err) {
-        setError((err as Error).message);
-      }
-    };
-
-    fetchRounds();
-    fetchMatches();
-  }, []);
-
-  useEffect(() => {
-    if (matches && svgRef.current) {
-      // Clear existing SVG to prevent duplicates
+    if (svgRef.current) {
       if (drawRef.current) {
-        drawRef.current.clear(); // Clear the existing SVG before drawing new elements
+        drawRef.current.clear();
       } else {
         drawRef.current = SVG()
           .addTo(svgRef.current)
-          .size(`${svgWidth}px`, `${svgHeight}px`); // Initialize the svg.js instance only once
+          .size(`${svgWidth}px`, `${svgHeight}px`);
       }
 
       if (drawRef.current && rounds) {
-        drawRef.current.rect("100%", "100%").fill("#f0f0f0"); // Draw rectangle as background
+        drawRef.current.rect("100%", "100%").fill("#2f0040"); // Updated background color
 
         const numberOfLines = rounds * 2;
         const matchWidth = svgWidth / (numberOfLines + 2);
@@ -145,10 +158,12 @@ export default function Home() {
             const y = 2 * matchHeight * (match - 1) + yStart;
 
             // draw top line of match
-            drawPlayerLine(drawRef, xLeftLeft, y, matchWidth, generateRandomString(10));
+            const player1 = getRandomPlayer();
+            drawPlayerLine(drawRef, xLeftLeft, y, matchWidth, player1.username, player1.image, 0);
 
             // draw bottom line of match
-            drawPlayerLine(drawRef, xLeftLeft, y + matchHeight, matchWidth, generateRandomString(10));
+            const player2 = getRandomPlayer();
+            drawPlayerLine(drawRef, xLeftLeft, y + matchHeight, matchWidth, player2.username, player2.image, 0);
 
             // draw connecting line of match
             drawVerticalLine(drawRef, xLeftRight, y, matchHeight);
@@ -161,10 +176,12 @@ export default function Home() {
             // console.log("y: ", y);
 
             // draw top line of match
-            drawPlayerLine(drawRef, xRightLeft, y, matchWidth, generateRandomString(10));
+            const player1 = getRandomPlayer();
+            drawPlayerLine(drawRef, xRightLeft, y, matchWidth, player1.username, player1.image, 0);
 
             // draw bottom line of match
-            drawPlayerLine(drawRef, xRightLeft, y + matchHeight, matchWidth, generateRandomString(10));
+            const player2 = getRandomPlayer();
+            drawPlayerLine(drawRef, xRightLeft, y + matchHeight, matchWidth, player2.username, player2.image, 0);
 
             // draw connecting line of match
             drawVerticalLine(drawRef, xRightLeft, y, matchHeight);
@@ -174,10 +191,12 @@ export default function Home() {
         }
 
         // Draw left side of final match
-        drawPlayerLine(drawRef, (rounds - 1) * matchWidth, svgHeight/2, matchWidth, generateRandomString(10));
+        const player1 = getRandomPlayer();
+        drawPlayerLine(drawRef, (rounds - 1) * matchWidth, svgHeight/2, matchWidth, player1.username, player1.image, 0);
 
         // Draw right side of final match
-        drawPlayerLine(drawRef, (rounds + 2) * matchWidth, svgHeight/2, matchWidth, generateRandomString(10));
+        const player2 = getRandomPlayer();
+        drawPlayerLine(drawRef, (rounds + 2) * matchWidth, svgHeight/2, matchWidth, player2.username, player2.image, 0);
 
         // Draw winner box
         const winnerBoxHeight = svgHeight / 6;
@@ -185,22 +204,22 @@ export default function Home() {
           .rect(matchWidth * 2, winnerBoxHeight)
           .move(rounds * matchWidth, svgHeight / 2 - winnerBoxHeight / 2) // Position the box at xLeft and y
           .fill("none")
-          .stroke({ width: 2, color: "#000000" }); // Black stroke with width 2
+          .stroke({ width: 2, color: "#e59eff" }); // Updated stroke color
       }
     }
-  }, [matches, rounds]);
+  }, [rounds]);
 
   return (
     <div>
-      <h1>Match Bracket</h1>
+      {/* <h1>Match Bracket</h1>
       {error && <p>{error}</p>}
       {!matches ? (
         <p>Loading matches...</p>
-      ) : matches.length > 0 ? (
-        <div ref={svgRef}></div> // SVG container for the bracket
-      ) : (
+      ) : matches.length > 0 ? ( */}
+        <div ref={svgRef}></div>  {/* SVG container for the bracket */}
+      {/* ) : (
         <p>No matches found</p>
-      )}
+      )} */}
     </div>
   );
 }
